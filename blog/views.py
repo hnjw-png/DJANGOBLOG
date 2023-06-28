4e2e1b4f6fd78400ca87bc31ecd93291ac1ca648
Holly Williams <hollyyeh12@gmail.com>
2023-06-28 17:33:46 +0000

    add templates

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django. shortcuts import render
 from django. views import generic
-from models. import Post
+from .models import Post
 
 
 # Create your views here.
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by[-created_on]
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
-
--- bookingblog/settings.py
+++ bookingblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-hnjwpng-djangoblog-80i5aid5yoa.ws-eu100.gitpod.io']
+ALLOWED_HOSTS = ['8000-hnjwpng-djangoblog-80i5aid5yoa.ws-eu101.gitpod.io']
 
 
 # Application definition
