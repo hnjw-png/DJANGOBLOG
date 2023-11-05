ca0e25bca3d2dce739328982b1d2f11209acd502
Holly Williams <hollyyeh12@gmail.com>
2023-11-05 12:43:09 +0000

    last commit of procfile, settings.py before deployment

--- bookingblog/settings.py
+++ bookingblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-hnjwpng-djangoblog-80i5aid5yoa.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['rest.herokuapp.com']
 
 
 # Application definition
@@ -129,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+ 
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
