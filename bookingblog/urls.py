7d1d4d927ecd508e63594e037c93c382c2d0fc4c
Holly Williams <hollyyeh12@gmail.com>
2023-11-07 12:40:26 +0000

    add urls to framework, so that the booking page for the reservation and booking submit model appears

--- bookingblog/urls.py
+++ bookingblog/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog import views 
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
-    path('booking-submit', views.bookingSubmit, name='bookingSubmit'),
-    path('Reservation', views.Reservation, name='Reservation'),
+    path('booking-submit', views.bookingSubmit, name='booking.html'),
+    path('Reservation', views.Reservation, name='booking.html'),
 ]
