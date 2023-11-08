720f4a0ff35fc51fdccc882d950de568ca3432cf
Holly Williams <hollyyeh12@gmail.com>
2023-11-08 15:59:24 +0000

    correcting urls to make reservation system appear.

--- bookingblog/urls.py
+++ bookingblog/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
-from blog import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
