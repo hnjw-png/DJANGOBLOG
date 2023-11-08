f4261338b64718082baf3777a5dd01c6d91ae2c9
Holly Williams <hollyyeh12@gmail.com>
2023-11-08 13:42:02 +0000

    create html templates for reservation details, form and list.

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Event
+
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
--- blog/views.py
+++ blog/views.py
@@ -3,69 +3,35 @@ from django. views import generic
 from .models import Post
 from datetime import datetime, timedelta
 from django.contrib import messages
-from .models import Reservation, Table, Client
+from .models import Reservation, Client
 from .forms import EventForm
 import login_required
 
 
-def Reservation(request):
-    weekdays = validWeekday(30)
+def reservation_list(request):
+    Reservation = reservation.objects.all()
+    return render(request, 'bookingblog/reservation_list.html', {'reservation': Reservation})
 
-    validateWeekdays = isWeekdayValid(weekdays)
+def reservation_detail(request, reservation_id):
+    reservation = get_object_or_404(Event, pk=reservation_id)
+    Client = Client.objects.filter(reservation=reservation)
+    return render(request, 'bookingblog/reservation_detail.html', {'reservation': Reservation, 'client': Client})
 
-    if request.method == 'Post':
-        service = request.POST.get('service')
-        day = request.POST.get('day')
-        if service == None:
-            messages.success(request, "Please Choose the type of event!")
-            return redirect('booking.html')
-
-        request.session['day'] = day
-        request.session['service'] = service
-
-        return redirect('bookingSubmit')
-
-    return render(request, 'booking.html', {
-        'weekdays':weekdays,
-        'validateWeekdays':validateWeekdays,
-    })
-
-
-def bookingSubmit(request):
-    user = request.user
-    times = [
-        "2pm-12pm", "10am-2pm", "10am-2pm"
-    ]
-    today = datetime.now()
-    minDate = today.strftime('%Y-%M-%D')
-    deltatime = today + timedelta(days=30)
-    strdeltatime = deltatime.strftime('%Y-%M-%D')
-    maxDate = strdeltatime
-
-    return render(request, 'booking.html', {})
-
-
-def event_list(request):
-    events = Event.objects.all()
-    return render(request, 'event/event_list.html', {'events': events})
-def event_detail(request, event_id):
-    event = get_object_or_404(Event, pk=event_id)
-    attendees = Attendee.objects.filter(event=event)
-    return render(request, 'event/event_detail.html', {'event': event, 'attendees': attendees})
 @login_required
-def create_event(request):
+def create_reservation(request):
     if request.method == 'POST':
         form = EventForm(request.POST)
         if form.is_valid():
-            event = form.save(commit=False)
-            event.organizer = request.user
-            event.save()
-            return redirect('event_detail', event_id=event.id)
+            reservation = form.save(commit=False)
+            reservation.organizer = request.user
+            reservation.save()
+            return redirect('reservation_detail', reservation_id=reservation.id)
     else:
-        form = EventForm()
-    return render(request, 'event/event_form.html', {'form': form})
+        form = ReservationForm()
+    return render(request, 'blog/reservation_form.html', {'form': form})
+
 @login_required
-def register_event(request, event_id):
+def register_reservation(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     Attendee.objects.get_or_create(user=request.user, event=event)
-    return redirect('event_detail', event_id=event.id)
+    return redirect('reservation_detail', reservation_id=reservation.id)
