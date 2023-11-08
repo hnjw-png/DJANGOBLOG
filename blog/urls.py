ef31ec9dd1ea5928cd2dc8bafed454e0b44e2906
Holly Williams <hollyyeh12@gmail.com>
2023-11-08 16:55:49 +0000

    update urls an views, to make reservation form appear

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,10 @@ from blog.views import register_reservation, reservation_detail, reservation_lis
 
 
 urlpatterns = [
-    path('index/', register_reservation, name='register_reservation'),
-    path('index/', reservation_list, name='reservation_list'),
-    path('index/', reservation_detail, name='reservation_detail'),    
-    path('index/', create_reservation, name='create_reservation'),
+    path('index.html/', register_reservation, name='register_reservation'),
+    path('index.html/', reservation_list, name='reservation_list'),
+    path('index.html/', reservation_detail, name='reservation_detail'),    
+    path('index.html/', create_reservation, name='create_reservation'),
 
     
 ]
