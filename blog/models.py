f03fd27338e556a4db71cd4c6059bf71beee7baf
Holly Williams <hollyyeh12@gmail.com>
2023-11-11 14:40:41 +0000

    edit models to fit more appripriately to a events page.

--- blog/models.py
+++ blog/models.py
@@ -18,16 +18,31 @@ TIME_CHOICES = (
     
  )
 
-
 class Reservation(models.Model):
-    place = models.DateField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="undecided")
-    time = models.CharField(max_length=15, choices=TIME_CHOICES, default="3pm")
+    title = models.CharField(max_length=200)
+    service = models.TextField(max_length =100, choices=SERVICE_CHOICES, default="undecided")
+    date = models.DateField()
+    time = models.TimeField(choices=TIME_CHOICES, default="3pm")
+    location = models.CharField(max_length=200)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
 class Client(models.Model):
-    user = user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     def __str__(self):
-        return f"{self.user.username} | day: {self.place} | time: {self.time}"
+        return f"{self.user.username} - {self.event.title}"
+
+#class Reservation(models.Model):
+ #   place = models.DateField()
+  #  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+   # service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="undecided")
+    # time = models.CharField(max_length=15, choices=TIME_CHOICES, default="3pm")
+
+#class Client(models.Model):
+ #   user = user = models.ForeignKey(User, on_delete=models.CASCADE)
+  #  Reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+
+   # def __str__(self):
+    #    return f"{self.user.username} | day: {self.place} | time: {self.time}"
