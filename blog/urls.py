b4b675b91dfca3dcd21b567d7afd52f1c6e17c5a
Holly Williams <hollyyeh12@gmail.com>
2023-11-11 12:41:39 +0000

    edit views.py , as views not correct, will test now.

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
-from blog.views import Reservation, register_reservation, reservation_detail, reservation_list, create_reservation
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('index/', Reservation, name='reservation'),
-    path('', register_reservation, name='register_reservation'),
-    path('', reservation_list, name='reservation_list'),
-    path('', reservation_detail, name='reservation_detail'),    
-    path('', create_reservation, name='create_reservation'),
-
-    
+    path('', views.reservation_list, name='reservation_list'),
+    path('reservation/<int:reservation_id>/', views.reservation_detail, name='reservation_detail'),
+    path('create_reservation/', views.create_reservation, name='create_reservation'),
+    path('register_reservation/<int:reservation_id>/', views.register_reservation, name='register_rservation'),
 ]
