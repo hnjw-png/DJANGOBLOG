550a27a680f5a9480e6d21a7d222d76d45297b07
Holly Williams <hollyyeh12@gmail.com>
2023-11-11 14:42:15 +0000

    change form field names to fit models variable names.

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from .models import Reservation
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = ['place', 'user', 'service', 'time']
+        fields = ['title', 'description', 'date', 'time', 'location']
