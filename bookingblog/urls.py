150bf9527f1498f89b1c7fd807c64a7aa2d4e09c
Holly Williams <hollyyeh12@gmail.com>
2023-11-15 20:15:05 +0000

    edit base html, check urls.py

--- bookingblog/urls.py
+++ bookingblog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
+    
 ]
