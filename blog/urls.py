d2dbfa15cf4bd44a95b37899db3092d524a8ad55
Holly Williams <hollyyeh12@gmail.com>
2023-11-15 18:34:01 +0000

    add href links to nav bar.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('', views.reservation_list, name='reservation_list'),
     path('reservation/<int:reservation_id>/', views.reservation_detail, name='reservation_detail'),
     path('create_reservation/', views.create_reservation, name='create_reservation'),
     path('register_reservation/<int:reservation_id>/', views.register_reservation, name='register_reservation'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
