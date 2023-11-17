fea506e37b72979246b48d2e0a13e48854aa19f6
Holly Williams <hollyyeh12@gmail.com>
2023-11-17 17:23:12 +0000

    fix login and logout views.

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('reservation/<int:reservation_id>/', views.reservation_detail, name='reservation_detail'),
     path('create_reservation/', views.create_reservation, name='create_reservation'),
     path('register_reservation/<int:reservation_id>/', views.register_reservation, name='register_reservation'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
